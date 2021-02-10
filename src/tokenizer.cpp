29d9b9b57f98d83224d6a2d95cc7a74ffb14d301
Søren K.S. Gregersen <s.gregersen@gmail.com>
2021-02-10 17:11:33 +0100

    Very first tokenizer.

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 int main() {
     auto short_welcome = "Welcome to the tokenizer.";
@@ -18,18 +19,54 @@ int main() {
         std::getline(std::cin, next_line);
         std::cout << prompt << std::flush
     ) {
-        for (size_t i = 0; i < next_line.length();) {
-            int code_point_len = 1;
-            if ((next_line[i] & 0xf8) == 0xf0)             code_point_len = 4;
-            else if ((next_line[i] & 0xf0) == 0xe0)        code_point_len = 3;
-            else if ((next_line[i] & 0xe0) == 0xc0)        code_point_len = 2;
-            if ((i + code_point_len) > next_line.length()) code_point_len = 1;
-
-            std::string utf8_char = next_line.substr(i, code_point_len);
-            std::cout << utf8_char << " [" << code_point_len << "]" << std::endl;
-
-            i += code_point_len;
+        size_t identifier_start = 0;
+        size_t position = 0;
+        for (; position < next_line.length(); position++) {
+            auto next_char = next_line[position];
+    
+            switch (next_char) {
+                case ' ':
+                case '\t':
+                    if (identifier_start < position) {
+                        size_t length = position - identifier_start;
+                        std::cout << next_line.substr(identifier_start, length) << std::endl;
+                    }
+                    identifier_start = position + 1;
+                    continue;
+                case '(':
+                case ')':
+                case '[':
+                case ']':
+                case '{':
+                case '}':
+                case '<':
+                case '>':
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '&':
+                case '|':
+                case '!':
+                case '=':
+                case '#':
+                case '@':
+                case '"':
+                case '\'':
+                    if (identifier_start < position) {
+                        size_t length = position - identifier_start;
+                        std::cout << next_line.substr(identifier_start, length) << std::endl;
+                    }
+                    identifier_start = position + 1;
+                    std::cout << next_char << std::endl;
+                    continue;
+            }
         }
+        if (identifier_start < position) {
+            size_t length = position - identifier_start;
+            std::cout << next_line.substr(identifier_start, length) << std::endl;
+        }
+        std::cout << "EOL" << std::endl;
     }
 
     std::cout << "Exiting REPL..." << std::endl;
