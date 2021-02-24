9cbbb5b7936f537d1f821b97cf99b9dc146e7549
Søren K.S. Gregersen <s.gregersen@gmail.com>
2021-02-24 09:21:12 +0100

    Refactored into stream struct.

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -12,13 +12,44 @@
 // c =3
 // a+b * c
 
+struct stream {
+    const char* const begin_position;
+    const char* current_position;
+    int current_char_length;
+    const char* const end_position;
+
+    static stream from_string(const std::string str) {
+        return {str.data(), str.data(), 1, str.data() + str.length()};
+    }
+
+    bool not_at_end() { return current_position < end_position; }
+    char current_char(int offset = 0) { return current_position[offset]; }
+    void consume_current() { current_position += current_char_length; current_char_length = 1; }
+    int remaining_length() { return end_position - current_position; }
+    
+    bool consume_numeric() {
+        if (current_char() >= '0' && current_char() <= '9') {
+            consume_current();
+            return true;
+        }
+        return false;
+    }
+
+    bool consume_alpha() {
+        if (
+                (current_char() >= 'a' && current_char() <= 'z') ||
+                (current_char() >= 'A' && current_char() <= 'Z') ||
+                (current_char() == '_')
+        ) {
+            consume_current();
+            return true;
+        }
+        return false;
+    }
+};
+
 template <int N>
-bool read_utf8_id(
-    char* (&current_char),
-    int &code_length,
-    const char* last_char,
-    const uint32_t (&unicode_table)[N][2]
-) {
+bool consume_good_utf8(stream &str, const uint32_t (&unicode_table)[N][2]) {
     // multibyte test: 0b1xxxxxxx;
     static const uint8_t UTF8_MULTIBYTE_MASK = 0x80;  // 0b10000000;
 
@@ -39,48 +70,47 @@ bool read_utf8_id(
 
     static const uint8_t UTF8_NEXT_BYTE_MASK = 0x3f;  // 0b00111111;
 
-    if (!(*current_char & UTF8_MULTIBYTE_MASK))
-        return 0;
+    if (!(str.current_char() & UTF8_MULTIBYTE_MASK))
+        return false;
 
     uint32_t code = 0;
-    int remaining_length = last_char - current_char;
-    if ((*current_char & UTF8_4BYTE_MASK) == UTF8_4BYTE_TEST) {
-        if (remaining_length < 4 ) {
+    if ((str.current_char() & UTF8_4BYTE_MASK) == UTF8_4BYTE_TEST) {
+        if (str.remaining_length() < 4 ) {
             std::cerr << "UTF8 string error (missing bytes)" << std::endl;
         }
-        uint32_t b0 = (current_char[0] & UTF8_4BYTE_MASK_INV) << 18;
-        uint32_t b1 = (current_char[1] & UTF8_NEXT_BYTE_MASK) << 12;
-        uint32_t b2 = (current_char[2] & UTF8_NEXT_BYTE_MASK) << 6;
-        uint32_t b3 = (current_char[3] & UTF8_NEXT_BYTE_MASK) << 0;
+        uint32_t b0 = (str.current_char(0) & UTF8_4BYTE_MASK_INV) << 18;
+        uint32_t b1 = (str.current_char(1) & UTF8_NEXT_BYTE_MASK) << 12;
+        uint32_t b2 = (str.current_char(2) & UTF8_NEXT_BYTE_MASK) << 6;
+        uint32_t b3 = (str.current_char(3) & UTF8_NEXT_BYTE_MASK) << 0;
         code = b0 | b1 | b2 | b3;
-        code_length = 4;
+        str.current_char_length = 4;
     }
-    else if ((*current_char & UTF8_3BYTE_MASK) == UTF8_3BYTE_TEST) {
-        if (remaining_length < 3 ) {
+    else if ((str.current_char() & UTF8_3BYTE_MASK) == UTF8_3BYTE_TEST) {
+        if (str.remaining_length() < 3 ) {
             std::cerr << "UTF8 string error (missing bytes)" << std::endl;
         }
-        uint32_t b0 = (current_char[0] & UTF8_3BYTE_MASK_INV) << 12;
-        uint32_t b1 = (current_char[1] & UTF8_NEXT_BYTE_MASK) << 6;
-        uint32_t b2 = (current_char[2] & UTF8_NEXT_BYTE_MASK) << 0;
+        uint32_t b0 = (str.current_char(0) & UTF8_3BYTE_MASK_INV) << 12;
+        uint32_t b1 = (str.current_char(1) & UTF8_NEXT_BYTE_MASK) << 6;
+        uint32_t b2 = (str.current_char(2) & UTF8_NEXT_BYTE_MASK) << 0;
         code = b0 | b1 | b2;
-        code_length = 3;
+        str.current_char_length = 3;
     }
-    else if ((*current_char & UTF8_2BYTE_MASK) == UTF8_2BYTE_TEST) {
-        if (remaining_length < 2 ) {
+    else if ((str.current_char() & UTF8_2BYTE_MASK) == UTF8_2BYTE_TEST) {
+        if (str.remaining_length() < 2 ) {
             std::cerr << "UTF8 string error (missing bytes)" << std::endl;
         }
-        uint32_t b0 = (current_char[0] & UTF8_2BYTE_MASK_INV) << 6;
-        uint32_t b1 = (current_char[1] & UTF8_NEXT_BYTE_MASK) << 0;
+        uint32_t b0 = (str.current_char(0) & UTF8_2BYTE_MASK_INV) << 6;
+        uint32_t b1 = (str.current_char(1) & UTF8_NEXT_BYTE_MASK) << 0;
         code = b0 | b1;
-        code_length = 2;
+        str.current_char_length = 2;
     }
     else {
-        std::cerr << "Unknown UTF8 character '" << *current_char << "'" << std::endl;
+        std::cerr << "ERROR: Bad UTF8 code point '" << str.current_char() << "'" << std::endl;
     }
     
     for (size_t i = 0; i < N; i++) {
         if (code >= unicode_table[i][0] && code <= unicode_table[i][1]) {
-            current_char += code_length;
+            str.consume_current();
             return true;
         }
     }
@@ -101,21 +131,19 @@ int main() {
 
     
     for ( // Infinite REPL loop
-        std::string line_str;
-        std::getline(std::cin, line_str);
+        std::string line;
+        std::getline(std::cin, line);
         std::cout << prompt << std::flush
     ) {
-        char* first_char = line_str.data();
-        char* current_char = line_str.data();
-        char* last_char = line_str.data() + line_str.length();
-        while (current_char < last_char) {
+        auto str = stream::from_string(line);
+        while (str.not_at_end()) {
     
             // Single (byte) punctuation characters
-            switch (*current_char) {
+            switch (str.current_char()) {
                 case ' ':
                 case '\t':
-                    current_char++;
-                    continue; // gooble, gooble
+                    str.consume_current();
+                    continue;
                 case '(':
                 case ')':
                 case '+':
@@ -123,79 +151,44 @@ int main() {
                 case '*':
                 case '/':
                 case '=':
-                    std::cout << "PCT @ col:" << int(current_char - first_char) + 1;
-                    std::cout << " :: '" << *current_char << "'" << std::endl;
-                    current_char++;
+                    std::cout << "PCT @ col:" << int(str.current_position - str.begin_position) + 1;
+                    std::cout << " :: '" << str.current_char() << "'" << std::endl;
+                    str.consume_current();
                     continue;
             }
 
             // Number literal
-            auto id_start = current_char;
-            if (*current_char >= '0' && *current_char <= '9') {
-                current_char++;
-                for (;current_char < last_char; current_char++) {
-                    if (!(*current_char >= '0' && *current_char <= '9')) {
-                        break;
-                    }
-                }
-            }
+            auto id_start = str.current_position;
+            while (str.consume_numeric());
             // Number literal, decimal/fractional part
-            if (*current_char == '.') {
-                current_char++;
-                for (;current_char < last_char; current_char++) {
-                    if (!(*current_char >= '0' && *current_char <= '9')) {
-                        break;
-                    }
-                }
+            if (str.current_char() == '.') {
+                str.consume_current();
+                while (str.consume_numeric());
             }
             
-            if (id_start != current_char) { // Number literal has been parsed
-                int length = current_char - id_start;
+            if (id_start != str.current_position) { // Number literal has been parsed
+                int length = str.current_position - id_start;
                 auto number = std::string_view(id_start, length);
-                std::cout << "NUM @ col:" << int(id_start - first_char) + 1 << ", len:" << length;
+                std::cout << "NUM @ col:" << int(id_start - str.begin_position) + 1 << ", len:" << length;
                 std::cout << " :: '" << number << "'" << std::endl;
                 continue;
             }
 
-            int code_length = 1;
-            bool is_id_start = (
-                (*current_char >= 'a' && *current_char <= 'z') ||
-                (*current_char >= 'A' && *current_char <= 'Z') ||
-                (*current_char == '_')
-            );
-            if (is_id_start)
-                current_char++;
-            else
-                is_id_start = read_utf8_id(current_char, code_length, last_char, id_start_table);
+            // identifier
+            str.current_char_length = 1;
+            if (str.consume_alpha() || consume_good_utf8(str, id_start_table))
+                while (str.consume_alpha() || consume_good_utf8(str, id_continue_table));
 
-            bool bad_utf8_char = false;
-            if (is_id_start) {
-                // identifier
-                bool is_id_continue = true;
-                while (is_id_continue && current_char < last_char) {
-                    is_id_continue = (
-                        (*current_char >= 'a' && *current_char <= 'z') ||
-                        (*current_char >= 'A' && *current_char <= 'Z') ||
-                        (*current_char >= '0' && *current_char <= '9') ||
-                        (*current_char == '_')
-                    );
-                    if (is_id_continue)
-                        current_char++;
-                    else
-                        is_id_continue = read_utf8_id(current_char, code_length, last_char, id_continue_table);
-                };
-            }
-
-            if (id_start != current_char) { // Identifier has been parsed
-                int length = current_char - id_start;
+            if (id_start != str.current_position) { // Identifier has been parsed
+                int length = str.current_position - id_start;
                 auto number = std::string_view(id_start, length);
-                std::cout << "SYM @ col:" << int(id_start - first_char) + 1 << ", len:" << length;
+                std::cout << "SYM @ col:" << int(id_start - str.begin_position) + 1 << ", len:" << length;
                 std::cout << " :: '" << number << "'" << std::endl;
                 continue;
             }
 
-            std::cerr << "Unknown character '" << std::string_view(current_char, code_length) << "'" << std::endl;
-            current_char += code_length;
+            std::cerr << "Unknown character '" << std::string_view(str.current_position, str.current_char_length) << "'" << std::endl;
+            str.consume_current();
         };
 
         std::cout << "EOL" << std::endl;
